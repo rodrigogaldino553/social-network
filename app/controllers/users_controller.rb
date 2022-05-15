f78b5357ef239fdc6d225fc1fd2ab9c207673ba0
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-05-15 16:16:40 -0300

    feat: add follow basics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :follow, :unfollow]
 
   def index
     @users = User.all
@@ -26,6 +26,18 @@ class UsersController < ApplicationController
   #     binding.pry
   #   end
   # end
+
+  def follow
+    # @user = User.find(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: user_path(@user))
+  end
+  
+  def unfollow
+    # @user = User.find(params[:id])
+    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    redirect_back(fallback_location: user_path(@user))
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :follows
+
   devise_for :users
   resources :users
+  post '/users/:id/follow', to: "users#follow", as: "follow_user"
+  post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+
   resources :pictures do
     resources :comments, except: [:index]
     member do
